a43072f8efce2509fd1d025503029d06a3f80d69
Erfan Ahmadi <levent.cpp@gmail.com>
2015-12-11 23:01:09 +0330

    Fixed

--- DXFramework/DXFramework/Window.cpp
+++ DXFramework/DXFramework/Window.cpp
@@ -1,52 +1,56 @@
 #include "Window.h"
-#include "WinUser.h"
+#include "stdafx.h"
 
 LRESULT Window::msgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	Window* window = 0;
 
+	window = getWindow(hwnd);
+
 	switch (msg)
 	{
 	case WM_CREATE:
-		window = (Window*)(getWindowLong(hwnd));
-		return 0;
+		break;
 
 	case WM_INPUT:
-		return 0;
+		break;
 
 	case WM_GETMINMAXINFO:
-		LPMINMAXINFO lpMMI = (LPMINMAXINFO)lParam;
-		lpMMI->ptMinTrackSize.x = window->getMinSize().x;
-		lpMMI->ptMinTrackSize.y = window->getMinSize().y;
-		return 0;
+		LPMINMAXINFO MMI = (LPMINMAXINFO)lParam;
+		MMI->ptMinTrackSize.x = window->getMinSize().x;
+		MMI->ptMinTrackSize.y = window->getMinSize().y;
+		break;
 
 	case WM_SIZE:
-		return 0;
+		break;
 
 	case WM_DESTROY:
 		PostQuitMessage(0);
-		return 0;
-
-	case WM_SYSCOMMAND:
-		if (wParam == SC_KEYMENU && (lParam >> 16) <= 0) return 0;
 		break;
 
+	case WM_SYSCOMMAND:
+		if (wParam == SC_KEYMENU && IsMenuActiveByAlt) return 0;
+		break;
+
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
 
-	return DefWindowProc(hwnd, msg, wParam, lParam);
+	return 0;
 }
 
-Window::Window(HINSTANCE instance, LPCSTR title, int width, int height) : Window(instance, (title), { width, height }) {}
-
-Window::Window(HINSTANCE instance, LPCSTR title, const XMINT2& size) : title_(title), size_(size)
+Window::Window(HINSTANCE instance, LPCSTR title, int width, int height) : title(title), size({width,height})
 {
+
 	WNDCLASSEX wc;
-	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	ZeroMemory(&wc, sizeof(wc));
 
 	wc.cbSize = sizeof(WNDCLASSEX);
 	wc.hInstance = instance;
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+	wc.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
 	wc.lpszClassName = "Window";
 	wc.style = CS_VREDRAW | CS_HREDRAW;
 	wc.lpfnWndProc = msgProc;
@@ -57,87 +61,90 @@ Window::Window(HINSTANCE instance, LPCSTR title, const XMINT2& size) : title_(ti
 
 	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
 
-	handle_ = CreateWindowEx(NULL,
-		"Window",
-		"DirectX Framework",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		wr.right - wr.left,
-		wr.bottom - wr.top,
-		NULL, NULL, instance, NULL);
+	handle = CreateWindowEx(NULL,
+							"Window",
+							title,
+							WS_OVERLAPPEDWINDOW,
+							CW_USEDEFAULT,
+							CW_USEDEFAULT,
+							wr.right - wr.left,
+							wr.bottom - wr.top,
+							nullptr,
+							nullptr,
+							instance,
+							nullptr);
 
-	::SetWindowLong(handle_, -21, long(this));
+	SetWindowLong(handle, GWL_USERDATA, (LONG)this);
 
-	if (!handle_)
+	if (!handle)
 		throw std::invalid_argument("Window Not Created");
 
-	ShowWindow(handle_, SW_SHOW);
-	UpdateWindow(handle_);
+	ShowWindow(handle, SW_SHOW);
 }
 
-void Window::getMousePosition(POINT* p)
+
+XMINT2 Window::getMousePosition()
 {
-	*p = { mouse_position_.x, mouse_position_.y };
+	return { mousePosition.x, mousePosition.y };
 }
 
 void Window::setTitle(LPCSTR title)
 {
-	title_ = title;
+	this->title = title;
 }
 
 void Window::setSize(int width, int height)
 {
-	size_ = { width, height };
+	size = { width, height };
 }
 
 void Window::setMinSize(int width, int height)
 {
-	min_size_ = { width, height };
+	minSize = { width, height };
 }
 
 void Window::setResizable(bool resizable)
 {
-	resizable_ = resizable_;
+	this->resizable = resizable;
 }
 
 void Window::setMaximizable(bool maximizable)
 {
-	maximizable_ = maximizable_;
+	this->maximizable = maximizable;
 }
 
-Window* Window::getWindowLong(HWND hwnd)
+Window* Window::getWindow(HWND hwnd)
 {
 	return (Window*)GetWindowLong(hwnd, -21);
 }
 
 LPCSTR Window::getTitle()
 {
-	return title_;
+	return title;
 }
 
-XMINT2 Window::getSize()
+XMINT2& Window::getSize()
 {
-	return size_;
+	return size;
 }
 
-XMINT2 Window::getMinSize()
+XMINT2& Window::getMinSize()
 {
-	return min_size_;
+	return minSize;
 }
 
 bool Window::isResizable()
 {
-	return resizable_;
+	return resizable;
 }
 
 bool Window::isMaximizable()
 {
-	return maximizable_;
+	return maximizable;
 }
 
 HWND Window::getHandle()
 {
-	return handle_;
+	return (HWND)GetModuleHandle(nullptr);
 }
 
--- DXFramework/DXFramework/Window.h
+++ DXFramework/DXFramework/Window.h
@@ -7,39 +7,42 @@
 #include "SwapChain.h"
 
 using DirectX::XMINT2;
+#define IsMenuActiveByAlt (lParam >> 16) <= 0 
 
 class Window
 {
-private:
-	LPCSTR title_;
-	XMINT2 size_;
-	XMINT2 min_size_;
-	XMINT2 mouse_position_;
-	bool maximizable_;
-	bool resizable_;
-	bool keys[NUM_KEYCODES];
-	HWND handle_;
-	SwapChain swap_chain_;
-
-	static LRESULT CALLBACK msgProc(HWND hwnd,
-		UINT msg,
-		WPARAM wParam, LPARAM lParam);
 public:
 	Window(HINSTANCE instance, LPCSTR title, int width, int height);
-	Window(HINSTANCE instance, LPCSTR title, const XMINT2 &size);
+	//Window(HINSTANCE instance, LPCSTR title, const XMINT2 &size);
+
 	void clear(float r, float g, float b, float a);
-	void getMousePosition(POINT *p);
+	XMINT2 getMousePosition();
 	void setTitle(LPCSTR title);
 	void setSize(int width, int height);
 	void setMinSize(int width, int height);
 	void setResizable(bool resizable);
 	void setMaximizable(bool maximizable);
 	bool isKeyPressed(Key key);
-	static Window* getWindowLong(HWND hwnd);
+	static Window *getWindow(HWND hwnd);
 	LPCSTR getTitle();
-	XMINT2 getSize();
-	XMINT2 getMinSize();
+	XMINT2& getSize();
+	XMINT2& getMinSize();
 	bool isResizable();
 	bool isMaximizable();
 	HWND getHandle();
+
+private:
+	LPCSTR title;
+	XMINT2 size;
+	XMINT2 minSize;
+	XMINT2 mousePosition;
+	bool maximizable;
+	bool resizable;
+	bool keys[NUM_KEYCODES];
+	HWND handle;
+	SwapChain swapChain;
+
+	static LRESULT CALLBACK msgProc(HWND hwnd,
+									UINT msg,
+									WPARAM wParam, LPARAM lParam);
 };
